66cfe9b57ec4ac4e13740af4c57de7923a4375f0
wolf813 <danielemorales7@gmail.com>
2022-12-09 23:18:24 -0600

    detalleartentrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/mantenimiento' },
   { path: 'mantenimiento', loadChildren: () => import('./mantenimiento/mantenimiento.module').then(m => m.MantenimientoModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
